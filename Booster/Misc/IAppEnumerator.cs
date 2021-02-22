5fefa136e11d352f0b6b04c26f95aeb58f409910
Flux <79461618+devfluxo@users.noreply.github.com>
2021-02-22 21:05:07 +0100

    Add files via upload

---
+++ Booster/Misc/IAppEnumerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Booster.Misc
+{
+    public interface IAppEnumerator : IDisposable
+    {
+        void Init(List<string> apps);
+        IEnumerable<uint> Enumerate();
+    }
+}
---
+++ Booster/Misc/Win32_PrivilegeElevation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace Booster.Misc
+{
+    internal class Win32_PrivilegeElevation
+    {
+        [DllImport("advapi32.dll", SetLastError = true)]
+        internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall, ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
+
+        public static bool SetIncreasePrivilege(string privilegeName)
+        {
+            bool result;
+            using (WindowsIdentity current = WindowsIdentity.GetCurrent(TokenAccessLevels.Query | TokenAccessLevels.AdjustPrivileges))
+            {
+                TokPriv1Luid newst;
+                newst.Count = 1;
+                newst.Luid = 0L;
+                newst.Attr = 2;
+                if (!Win32_PrivilegeElevation.LookupPrivilegeValue(null, privilegeName, ref newst.Luid))
+                {
+                    throw new Exception("Error in LookupPrivilegeValue: ", new Win32Exception(Marshal.GetLastWin32Error()));
+                }
+                int num = Win32_PrivilegeElevation.AdjustTokenPrivileges(current.Token, false, ref newst, 0, IntPtr.Zero, IntPtr.Zero) ? 1 : 0;
+                if (num == 0)
+                {
+                    throw new Exception("Error in AdjustTokenPrivileges: ", new Win32Exception(Marshal.GetLastWin32Error()));
+                }
+                result = (num != 0);
+            }
+            return result;
+        }
+
+        private const int SE_PRIVILEGE_ENABLED = 2;
+        private const string SE_INCREASE_QUOTA_NAME = "SeIncreaseQuotaPrivilege";
+        public const string SE_PROFILE_SINGLE_PROCESS_NAME = "SeProfileSingleProcessPrivilege";
+    }
+}
